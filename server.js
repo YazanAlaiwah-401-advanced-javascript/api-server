4f9e93105fb7f8d6ca0f88c3596b1540091a5102
YazanAlaiwah <eng.yazanalaiwah@gmail.com>
2020-05-31 21:41:21 +0300

    done

---
+++ server.js
@@ -0,0 +1,20 @@
+// 'use strict';
+
+// const jsonServer = require('json-server')
+// const server = jsonServer.create()
+// const router = jsonServer.router('db.json')
+// const middlewares = jsonServer.defaults()
+// server.use(middlewares)
+// server.use(router)
+
+
+// router.render = (req, res) => {
+//   res.jsonp({
+//     body: res.locals.data;
+//   });
+// };
+
+
+// server.listen(3000, () => {
+//   console.log('JSON Server is running')
+// })
