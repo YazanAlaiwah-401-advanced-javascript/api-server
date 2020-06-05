b758162f029322ad588bc5124cb8e0b1aa7a920c
YazanAlaiwah <eng.yazanalaiwah@gmail.com>
2020-06-05 22:33:07 +0300

    add jsdoc

--- middleware/getmodel.js
+++ middleware/getmodel.js
@@ -1,6 +1,9 @@
 'use strict';
 const product = require('../lib/models/products/products.collection.js');
 const category = require('../lib/models/categories/categoies.collections.js');
+/**
+ * this middel ware to add the model collection in the request model 
+ */
 let getModel = (req,res,next)=>{
   let model = req.params.model;
   switch (model) {
